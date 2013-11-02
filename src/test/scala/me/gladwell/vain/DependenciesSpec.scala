5a6c4970698d9c8e96d46d1b7f3c35d4c6facd2c
Ricardo Gladwell <ricardo@gladwell.me>
2013-11-02 11:43:27 +0000

    Switched to Maven

--- src/test/scala/me/gladwell/vain/DependenciesSpec.scala
+++ src/test/scala/me/gladwell/vain/DependenciesSpec.scala
@@ -3,18 +3,22 @@ package me.gladwell.vain
 import org.scalatest.FlatSpec
 import org.scalatest.GivenWhenThen
 import org.scalatest.Matchers
-
 import me.gladwell.vain._
 import me.gladwell.vain.dependencies.JavaLibrary
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class DependenciesSpec extends FlatSpec with Matchers with GivenWhenThen with Classpath {
 
-  "A Vain module" should "resolve and add Java library dependencies to the classpath" in {
+  "A Vain module" should "resolve and add Java library dependencies to the runtime classpath" in {
     Given("a module with a dependency on the commons-lang Java library")
-    object TestModule extends Module {
-      println("hello, world!")
+    object testModule extends Module {
       dependsOn(Seq(JavaLibrary(group = "commons-lang", name = "commons-lang", version = "2.4")))
     }
+
+    When("the module is loaded")
+    testModule
 
     Then("the classpath should contain the commons-lang jar")
     exactly(1, classpath) should be (dependency(JavaLibrary(group = "commons-lang", name = "commons-lang", version = "2.4")))
